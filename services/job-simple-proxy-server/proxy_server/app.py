11b6922885783cbb6940591915bc5f9e504d2338
craftsangjae <craftsangjae@gmail.com>
2024-02-20 11:36:11 +0900

    add Error Handling for debugging easily

--- services/job-simple-proxy-server/proxy_server/app.py
+++ services/job-simple-proxy-server/proxy_server/app.py
@@ -1,10 +1,10 @@
-from typing import List
+from typing import List, Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from kubernetes import client, config
 from kubernetes.config.config_exception import ConfigException
+from kubernetes.client.exceptions import ApiException
 import logging
-import json
 
 from proxy_server.dto import JobInfo, CreateJobRequest, DeleteJobRequest, JobStatusResponse
 
@@ -15,7 +15,11 @@ try:
 except ConfigException:
     # run in local host
     logging.warning("Service host/port is not set. use load_kube_config instead")
-    config.load_kube_config()
+    try:
+        config.load_kube_config()
+    except ConfigException:
+        logging.warning("unable to test. this app should be launched on K8S Infra.")
+        raise
 
 # load k8s client
 batch_v1 = client.BatchV1Api()
@@ -29,10 +33,15 @@ async def health_check():
 
 
 @app.get('/jobs')
-async def list_jobs(namespace="default") -> List[JobInfo]:
+async def list_jobs(response: Response, namespace="default") -> Union[List[JobInfo], str]:
     """ returns the list of jobs
     """
-    ret = batch_v1.list_namespaced_job(namespace=namespace)
+    try:
+        ret = batch_v1.list_namespaced_job(namespace=namespace)
+    except ApiException as e:
+        response.status_code = 403
+        return f"ApiException : {e.reason}"
+
     return [
         JobInfo(name=item.metadata.name, namespace=item.metadata.namespace,
                 active=item.status.active, failed=item.status.failed,
@@ -41,19 +50,29 @@ async def list_jobs(namespace="default") -> List[JobInfo]:
 
 
 @app.post("/jobs", status_code=201)
-async def create_job(req: CreateJobRequest):
+async def create_job(req: CreateJobRequest, response: Response) -> Union[JobStatusResponse, str]:
     """ create job
     """
     job_object = create_job_object(req.name, req.value)
-    res = batch_v1.create_namespaced_job(req.namespace, job_object)
+    try:
+        res = batch_v1.create_namespaced_job(req.namespace, job_object)
+    except ApiException as e:
+        response.status_code = 403
+        return f"ApiException : {e.reason}"
+
     return JobStatusResponse(status=str(res.status))
 
 
 @app.delete("/jobs")
-async def delete_job(req: DeleteJobRequest):
+async def delete_job(req: DeleteJobRequest, response: Response) -> Union[JobStatusResponse, str]:
     """ delete specific job
     """
-    res = batch_v1.delete_namespaced_job(req.name, req.namespace)
+    try:
+        res = batch_v1.delete_namespaced_job(req.name, req.namespace)
+    except ApiException as e:
+        response.status_code = 403
+        return f"ApiException : {e.reason}"
+    
     return JobStatusResponse(status=str(res.status))
 
 
